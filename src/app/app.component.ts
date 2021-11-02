19965a80a149c2c7dd6d057558dc969f4a591a49
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-11-02 22:42:01 +0200

    feat: makes api call on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector   : 'app-root',
   templateUrl: './app.component.html',
   styleUrls  : ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'angular-universal-starter';
+  users$: Observable<any> | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.users$ = this.http.get('api/users');
+  }
 }
